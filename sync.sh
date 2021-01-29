e9c1873ecb634ff4d17f2651d1a170459c3c26f7
Finn Sauer <fsauer@cock.li>
2021-01-29 14:41:36 +0100

    Sync: add script
    This script is used for syncing between the server and this local copy.
    I keep a local copy here, make changes locally, and then upload them to
    the server; in the same manner as git works.

---
+++ sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Syncs the website to the server located at WEBSITE.
+#
+# This script expects the WEBSITE environmental variable to be set.  If
+# not set in ~/.*profile you could launch this script with like this:
+#
+#   WEBSITE=example.com ./sync.sh
+#
+# The nginx server configuration should look for /var/www/website
+# directory and the directory should be owned by the user 'finn'-- the
+# same user as the current local user.
+
+_this=$(readlink -f ${BASH_SOURCE[0]})
+_dir=${_this%/*}
+_webroot=$(readlink -f ~/web)
+_public=${_webroot}/public
+
+# If folder exist; remove it
+[[ ! -e ${_public} ]] && rm -rf ${_public}
+
+# Generate public folder
+cd ${_webroot}
+hugo
+
+# Upload to server
+rsync -zrvP --delete-after ${_public} ${WEBSITE}:/var/www/website
